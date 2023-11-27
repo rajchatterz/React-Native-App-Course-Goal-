af857859d9c0cb8500c5a42069bf7966867fcf2b
rajchatterz <iamraj.space@gmail.com>
2023-11-27 20:56:42 +0530

    done with the image

--- Component/Input.js
+++ Component/Input.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { StyleSheet } from 'react-native'
-import { View,TextInput,Button,Modal } from 'react-native'
+
+import { View,TextInput,Button,Modal,Image } from 'react-native'
 const Input = ({ input,closeModal, buttonHandler,value ,visible}) => {
     
   return (
       <Modal visible={visible} animationType='fade'>
           <View style={styles.inputContainer}>
+              <Image style={styles.image} source={require('../assets/Images/goal.png')}/>
               <TextInput onChangeText={input} value={value} style={styles.textInput} placeholder='Your Course Goal' />
               <Button title='cancel' onPress={closeModal}/>
               <View>
@@ -21,8 +23,8 @@ export default Input
 const styles = StyleSheet.create({
     inputContainer: {
         flex:1,
-        flexDirection: 'row',
-        justifyContent: 'space-evenly',
+
+
         gap: 15,
         alignItems: 'center',
         paddingBottom: 24,
@@ -35,5 +37,10 @@ const styles = StyleSheet.create({
         borderColor: '#cccccc',
         width: '70%',
         padding:8
-      },
+    },
+    image: {
+        width: 100,
+        height: 100,
+        margin:20
+      }
 })
